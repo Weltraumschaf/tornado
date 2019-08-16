ab86787d65901caa830492094c3ea1ef50e6a4f3
Sven Strittmatter <sven.strittmatter@iteratec.de>
2019-08-17 00:04:03 +0200

    Initial Spring Boot skeleton

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tornado
+
+This is my playground for fast evaluating stuff. It is based on latest [Spring Boot][spring-boot] and Docker.
+
+To build and run execute:
+
+```bash
+mvn clena package && java -jar target/java -jar target/tornado-*.jar
+```
+
+[spring-boot]: https://spring.io/guides/gs/spring-boot/
---
+++ src/main/java/de/weltraumschaf/tornado/HelloController.java
@@ -0,0 +1,14 @@
+package de.weltraumschaf.tornado;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, World!\r\n!";
+    }
+
+}
